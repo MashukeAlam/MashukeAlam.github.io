79b9c9b3365d8a3554f0592adc230d2a7f8098c6
Mashuke Alam Jim <jimmashuke@gmail.com>
2021-09-20 23:55:30 +0600

    sure na hoise naki

--- Conways_Game_Of_Life/a_star.js
+++ Conways_Game_Of_Life/a_star.js
@@ -10,7 +10,7 @@ let animation_go_on = 1;
 let WIDTH = innerWidth;
 let HEIGHT = innerHeight;
 feild.width = feild.height = 800;
-var cols = rows = 80;
+var cols = rows = 30;
 var forMouseClick = feild.width / cols;
 var w = feild.width / cols;
 var h = feild.height / rows;
@@ -23,6 +23,7 @@ const obstacleColor = 'black';
 let speed = 70;
 var pendingPoints = [];
 var processedPoints = [];
+let LIFE = false;
 
 var final_path = [];
 
@@ -37,8 +38,7 @@ function createGrid(x, y) {
             grid[i][j] = new Point(i, j);
         }
     }
-    start = grid[0][0];
-    end = grid[x - 1][y - 1];
+    
     return grid;
 }
 
@@ -60,16 +60,10 @@ function Point(x, y) {
     this.h = 0;
     this.prev = null;
     this.obstacle = false;
+    // this.obstacle = (Math.random() > 0.3) ? false : true;
+    this.alive = false;
 
-    this.determineObstacle = function () {
-        var r = Math.ceil(Math.random() * 4);
-        //console.log(r, r%3);
-
-        if (r % 3 === 0) {
-            this.obstacle = true;
-        }
-
-    }
+    
 
     this.createNeighbors = function () {
         var i = this.x;
@@ -79,8 +73,8 @@ function Point(x, y) {
         try { if (grid[i - 1][j] != undefined) { this.neightbours.push(grid[i - 1][j]); } } catch (err) { }
         try { if (grid[i][j + 1] != undefined) { this.neightbours.push(grid[i][j + 1]); } } catch (err) { }
         try { if (grid[i][j - 1] != undefined) { this.neightbours.push(grid[i][j - 1]); } } catch (err) { }
-        //try { if (grid[i + 1][j + 1] != undefined) { this.neightbours.push(grid[i+1][j + 1]); } } catch (err) { }
-        //try { if (grid[i - 1][j - 1] != undefined) { this.neightbours.push(grid[i-1][j - 1]); } } catch (err) { }
+        try { if (grid[i + 1][j + 1] != undefined) { this.neightbours.push(grid[i+1][j + 1]); } } catch (err) { }
+        try { if (grid[i - 1][j - 1] != undefined) { this.neightbours.push(grid[i-1][j - 1]); } } catch (err) { }
 
     }
 
@@ -101,318 +95,83 @@ function Point(x, y) {
 
 }
 
-function a_star() {
-    reset();
-    for (var i = 0; i < grid.length; i++) {
-        for (var j = 0; j < grid[i].length; j++) {
-            //grid[i][j].determineObstacle();
-            grid[i][j].createNeighbors();
-        }
-    }
-
-    start.obstacle = false;
-    end.obstacle = false;
-
-    pendingPoints.push(start);
-
-    animate_a();
-
-}
-function dhix_start() {
-    reset();
-    dontAnimateBlank = 1;
-    speed = slider.value;
-    for (var i = 0; i < grid.length; i++) {
-        for (var j = 0; j < grid[i].length; j++) {
-            //grid[i][j].determineObstacle();
-            grid[i][j].createNeighbors();
-        }
-    }
-
-    start.obstacle = false;
-    end.obstacle = false;
-
-    pendingPoints.push(start);
-
-    animate_d();
-
-}
-
-
-
-function animate_a() {
-
-    if (pendingPoints.length > 0) {
-        //console.log(DONE);
-
-        if (DONE != 1 && animation_go_on === 1) {
-            setTimeout(() => {
-                requestAnimationFrame(animate_a);
-
-            }, speed);
-        } else {
-            //console.log('ff');
-
-
-        }
-        var minF = Infinity;
-        var minIndx;
-        for (var i = 0; i < pendingPoints.length; i++) {
-            if (pendingPoints[i].f < minF) {
-                minF = pendingPoints[i].f;
-                minIndx = i;
-            }
-        }
-
-        if (pendingPoints[minIndx].x == end.x && pendingPoints[minIndx].y == end.y) {
-            //console.log('Done');
-            end.showPath();
-
-            animation_go_on = 0;
-            DONE = 1;
-            //window.cancelAnimationFrame(animate);
-        }
-
-        var current = pendingPoints[minIndx];
-        //console.log(current);
-
-
-
-
-        processedPoints.push(current);
-        pendingPoints.splice(minIndx, 1);
-        var currentNeigh = current.neightbours;
-        //console.log(currentNeigh);
-
-        for (var k = 0; k < currentNeigh.length; k++) {
-            //console.log('here');
-
-            var element = currentNeigh[k];
-            if (processedPoints.includes(element) || element.obstacle) {
-                //console.log('fff00');
-
-                continue;
-            }
-            tentative_g = current.g + 1;
-            //console.log(tentative_g);
-
-            var a = (element.x - end.x);
-            var b = (element.y - end.y);
-            element.f = element.g + Math.sqrt(a * a + b * b);
-
-            if (!pendingPoints.includes(element)) {
-                if (tentative_g < element.g) {
-                    element.g = tentative_g;
-                    pendingPoints.push(element);
-                } else {
-                    pendingPoints.push(element);
-                }
-
-            }
-
-            element.prev = current;
-
-
-        }
-
-
-    } else {
-        console.log('Didn\'t Find');
-
-    }
-    //console.log('animating');
-    c.fillStyle = 'white';
-    c.fillRect(0, 0, 500, 500);
-    for (var i = 0; i < grid.length; i++) {
-        for (var j = 0; j < grid[i].length; j++) {
-            if (grid[i][j].obstacle) {
-                grid[i][j].show(obstacleColor)
-            } else {
-                grid[i][j].show(boardColor);
-            }
-
-        }
-    }
-
-
-
-    for (var j = 0; j < pendingPoints.length; j++) {
-        pendingPoints[j].show(pendingColor);
-    }
-
-    for (var j = 0; j < processedPoints.length; j++) {
-        processedPoints[j].show(processedColor);
-    }
-
-    if (DONE == 1) {
-        for (var o = 0; o < final_path.length; o++) {
-            //console.log(o, final_path[o]);
-
-
-            final_path[o].show('blue');
-        }
-    }
-}
-
-function animate_d() {
-    //dontAnimateBlank = 0;
-    if (pendingPoints.length > 0) {
-        //console.log(DONE);
-
-        if (DONE != 1 && animation_go_on === 1) {
-            setTimeout(() => {
-                requestAnimationFrame(animate_d);
-            }, speed);
-        } else {
-            //console.log('ff');
-
-            return;
-        }
-        var minF = Infinity;
-        var minIndx;
-        for (var i = 0; i < pendingPoints.length; i++) {
-            if (pendingPoints[i].f < minF) {
-                minF = pendingPoints[i].f;
-                minIndx = i;
-            }
-        }
-
-        if (pendingPoints[minIndx].x == end.x && pendingPoints[minIndx].y == end.y) {
-            //console.log('Done');
-            //pendingPoints.splice(minIndx, 1);
-            end.showPath();
-
-            animation_go_on = 0;
-            DONE = 1;
-            //window.cancelAnimationFrame(animate);
-        }
-
-        var current = pendingPoints[minIndx];
-        //console.log(current);
-
-
-
-
-        processedPoints.push(current);
-        pendingPoints.splice(minIndx, 1);
-        var currentNeigh = current.neightbours;
-        //console.log(currentNeigh);
-
-        for (var k = 0; k < currentNeigh.length; k++) {
-            //console.log('here');
-
-            var element = currentNeigh[k];
-            if (processedPoints.includes(element) || element.obstacle) {
-                //console.log('fff00');
-
-                continue;
-            }
-            tentative_g = current.g + 1;
-            //console.log(tentative_g);
-
-
-            element.f = element.g + 0;
-
-            if (!pendingPoints.includes(element)) {
-                if (tentative_g < element.g) {
-                    element.g = tentative_g;
-                    pendingPoints.push(element);
-                } else {
-                    pendingPoints.push(element);
-                }
-
-            }
-
-            element.prev = current;
-
-
-        }
-
-
-    } else {
-        if (DONE == 1) return;
-        console.log('Didn\'t Find');
-
-    }
-    //console.log('animating');
-    c.fillStyle = 'white';
-    c.fillRect(0, 0, 500, 500);
-    for (var i = 0; i < grid.length; i++) {
-        for (var j = 0; j < grid[i].length; j++) {
-            if (grid[i][j].obstacle) {
-                grid[i][j].show(obstacleColor)
-            } else {
-                grid[i][j].show(boardColor);
-            }
-
-        }
-    }
-
-
-
-    for (var j = 0; j < pendingPoints.length; j++) {
-        pendingPoints[j].show(pendingColor);
-    }
-
-    for (var j = 0; j < processedPoints.length; j++) {
-        processedPoints[j].show(processedColor);
-    }
-
-    if (DONE == 1) {
-        for (var o = 0; o < final_path.length; o++) {
-            //console.log(o, final_path[o]);
-
-
-            final_path[o].show('blue');
-        }
-    }
-}
-
-
-
-
-
-function a_star_start() {
-    dontAnimateBlank = 1;
-    speed = slider.value;
-    //console.log(speed);
-    
-    a_star();
-}
-
-function stop_anim() {
-    animation_go_on = 0;
-}
-
 function reset_board() {
     window.location.reload();
     
 }
 
-function animate_begin() {
-    if (dontAnimateBlank != 1) {
-        console.log('animating blank');
+const next_gen = () => {
+    // console.log('here');
+    // let future_gen = grid;
+    let future_gen = createGrid(800, 800);
+    for (let row = 1; row < grid.length; row++) {
+        for (let col = 1; col < grid[row].length; col++) {
+            let currPoint = grid[row][col];
+            let currIsAlive = currPoint.obstacle;
+            // console.log(currPoint.neightbours);
+            let numberOfAliveNeighbors = 0;
 
-        requestAnimationFrame(animate_begin);
-        c.fillStyle = 'white';
-        c.fillRect(0, 0, 500, 500);
-        for (var i = 0; i < grid.length; i++) {
-            for (var j = 0; j < grid[i].length; j++) {
-                if (grid[i][j].obstacle) {
-                    grid[i][j].show(obstacleColor)
-                }else if(start == grid[i][j]) {
-                    grid[i][j].show(startColor);
-                }
-                else if(end == grid[i][j]){
-                    grid[i][j].show(endColor);
-                }
-                else {
-                    grid[i][j].show(boardColor);
-                }
-
+            for (let n = 0; n < currPoint.neightbours.length; n++) {
+                // console.log(currPoint.neightbours[n].obstacle);
+                if (currPoint.neightbours[n].obstacle) {
+                    // console.log(11);
+                    numberOfAliveNeighbors++;
+                } 
+            }
+            // console.log(numberOfAliveNeighbors);
+            if (currIsAlive && numberOfAliveNeighbors < 2) {
+                future_gen[row][col].obstacle = false;
+            } else if (currIsAlive && numberOfAliveNeighbors > 3) {
+                future_gen[row][col].obstacle = false;
+            } else if (!currIsAlive && numberOfAliveNeighbors == 3) {
+                future_gen[row][col].obstacle = true;
+                console.log('here');
+            } else {
+                future_gen[row][col].obstacle = grid[row][col].obstacle;
             }
         }
     }
+
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[row].length; col++) {
+            // console.log(grid[row][col].obstacle);
+            grid[row][col].obstacle = future_gen[row][col].obstacle;
+        }
+    }
+    
+}
+
+function animate_begin() {
+    setTimeout(() => {
+        if (dontAnimateBlank != 1) {
+            // console.log('animating blank');
+    
+            requestAnimationFrame(animate_begin);
+            c.fillStyle = 'white';
+            c.fillRect(0, 0, 800, 800);
+    
+            // next_gen();
+    
+            if (LIFE) {
+                next_gen();
+            }
+    
+            for (var i = 0; i < grid.length; i++) {
+                for (var j = 0; j < grid[i].length; j++) {
+                    if (grid[i][j].obstacle) {
+    
+                        grid[i][j].show(obstacleColor)
+    
+                    }
+                    else {
+                        grid[i][j].show(boardColor);
+                    }
+    
+                }
+            }
+        }
+    }, 700);
+    
 }
 animate_begin();
 
@@ -442,9 +201,23 @@ feild.addEventListener('click', event => {
         grid[x - 1][y - 1].show(endColor);
     }else {
         grid[x - 1][y - 1].obstacle = !grid[x - 1][y - 1].obstacle;
+        // grid[x - 1][y - 1].alive = !grid[x - 1][y - 1].alive;
+
     }
 
 
 });
+
+
+const start_life = () => {
+    for (var i = 0; i < grid.length; i++) {
+        for (var j = 0; j < grid[i].length; j++) {
+            grid[i][j].createNeighbors();
+        }
+    }
+    LIFE = true;
+    animate_begin();
+}
+
 
 
